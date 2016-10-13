8567c7cd32c50f76ce202ddef368cb82700dfadf
Joao Paulo <jpc.araujo@hotmail.com>
2016-10-12 23:20:27 -0300

    Listando com for

---
+++ testes/for_of.ts
@@ -0,0 +1,23 @@
+var listCar = ["Uno","Gol","Palio"];
+
+for(var car of listCar){
+    console.log(car);
+}
+
+console.log("\n");
+
+var colors = ["Vermelho","Verde","Azul"];
+
+colors['title'] = "Colors";
+
+console.log(colors);
+
+for (var i in colors){
+    console.log(i);
+}
+
+console.log("\n");
+
+for (var color of colors){
+    console.log(color);
+}

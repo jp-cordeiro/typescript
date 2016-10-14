565e0d7e3f0637748eae2ca048370bc8e024f699
Joao Paulo <jpc.araujo@hotmail.com>
2016-10-14 13:00:23 -0300

    testes com typescript

---
+++ testes/for_of/for_of.js
@@ -0,0 +1,18 @@
+var listCar = ["Uno", "Gol", "Palio"];
+for (var _i = 0, listCar_1 = listCar; _i < listCar_1.length; _i++) {
+    var car = listCar_1[_i];
+    console.log(car);
+}
+console.log("\n");
+var colors = ["Vermelho", "Verde", "Azul"];
+colors['title'] = "Colors";
+console.log(colors);
+for (var i in colors) {
+    console.log(i);
+}
+console.log("\n");
+for (var _a = 0, colors_1 = colors; _a < colors_1.length; _a++) {
+    var color = colors_1[_a];
+    console.log(color);
+}
+//# sourceMappingURL=for_of.js.map

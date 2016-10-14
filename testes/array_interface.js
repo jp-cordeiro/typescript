ec6be935d83e9a184eb11ce8685096766886e88f
Joao Paulo <jpc.araujo@hotmail.com>
2016-10-14 12:55:27 -0300

    Interfaces de funções e arrays

---
+++ testes/array_interface.js
@@ -0,0 +1,17 @@
+//Indica que a variável implementa a interface
+var add;
+add = function (varA, varB) {
+    return true;
+};
+var getColor;
+getColor = function (codColor, title) {
+    if (title) {
+        return { codColor: codColor, title: title };
+    }
+    return { codColor: codColor };
+};
+console.log(getColor("#0000", "Black"));
+var a;
+a = ["teste"];
+console.log(a);
+//# sourceMappingURL=array_interface.js.map
